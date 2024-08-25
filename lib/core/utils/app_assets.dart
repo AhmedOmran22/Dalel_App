fd4669af59c8ec51a1d0997deffd5840187442c5
AhmedOmran22 <mamdouhanahamada@gmail.com>
2024-08-25 10:07:42 +0300

    creating app structure and add colors and assets to it

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: prefer_single_quotes
+class AppAssets {
+  AppAssets._();
+  
+  static const String assetsSvgsOnBoarding1 = "assets/svgs/on_boarding1.svg";
+
+  static const String assetsSvgsOnBoarding2 = "assets/svgs/on_boarding2.svg";
+
+  static const String assetsSvgsOnBoarding3 = "assets/svgs/on_boarding3.svg";
+
+  static const String assetsSvgsSuccess = "assets/svgs/success.svg";
+}
+
--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,16 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const DalelApp());
 }
+
 class DalelApp extends StatelessWidget {
   const DalelApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp
-    (
-      home: Scaffold(),
+    return const MaterialApp(
+      home: Scaffold(
+        body: SizedBox(),
+      ),
     );
   }
 }
